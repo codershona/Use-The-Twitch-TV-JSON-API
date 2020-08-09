1e6ddae0b9a2789498e978472d91112b6d0b1e6b
codershona <falgunimst95@gmail.com>
2020-08-09 19:22:51 +0200

    my project setup is almost done

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 serviceWorker.unregister();
