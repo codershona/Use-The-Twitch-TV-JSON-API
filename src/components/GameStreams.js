d7de0d3fe37c7ab3f51f4f8df6318d6b6822f3be
codershona <falgunimst95@gmail.com>
2020-08-12 16:21:31 +0200

    game :id api is working perfectly

--- src/components/GameStreams.js
+++ src/components/GameStreams.js
@@ -42,8 +42,38 @@ function GameStreams({ match, location }) {
 
 	return(
 		<div>
-		<li>{viewers}</li>
-		<li>{location.state.gameID}</li>
+
+		<h1 className="text-center">{match.params.id} Streams</h1>
+      <h3 className="text-center">
+        <strong className="text-primary">{viewers}</strong> people currently
+        watching {match.params.id}
+      </h3>
+      <div className="row">
+        {streamData.map(stream => (
+          <div className="col-lg-4 col-md-6 col-sm-12 mt-5">
+            <div className="card">
+              <img className="card-img-top" src={stream.thumbnail_url} />
+              <div className="card-body">
+                <h5 className="card-title">{stream.user_name}</h5>
+                <div className="card-text">
+                  {stream.viewer_count} live viewers
+                </div>
+                <button className="btn btn-success">
+                  <a
+                    className="link"
+                    href={"https://twitch.tv/" + stream.user_name}
+                    target="_blank"
+                  >
+                    watch {stream.user_name}'s channel
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+		
+
 
 		</div>
 
